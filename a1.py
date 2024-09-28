3924f06e4cac7ce0d88dd9e625e4c3fb2a14cc29
Alexandra Lesan <alexandra.lesan@dawsoncollege.qc.ca>
2024-09-28 13:18:03 -0400

    Add space to input and change int() to round()

--- a1.py
+++ a1.py
@@ -1,21 +1,21 @@
 # Here, the user will input each of their grades
-lab = int(input("Enter the number of labs completed:"))
+lab = int(input("Enter the number of labs completed: "))
 if lab > 6:
     lab = 6
-quiz = int(input("Enter the number of quizzes completed:"))
+quiz = int(input("Enter the number of quizzes completed: "))
 if quiz > 6:
     quiz = 6
-assi1 = float(input("Enter grade for Assignment 1:"))
-assi2 = float(input("Enter grade for Assignment 2:"))
-assi3 = float(input("Enter grade for Assignment 3:"))
-assi4 = float(input("Enter grade for Assignment 4:"))
+assi1 = float(input("Enter grade for Assignment 1: "))
+assi2 = float(input("Enter grade for Assignment 2: "))
+assi3 = float(input("Enter grade for Assignment 3: "))
+assi4 = float(input("Enter grade for Assignment 4: "))
 
-mid1 = float(input("Enter grade for Midterm 1:"))
-mid2 = float(input("Enter grade for Midterm 2:"))
+mid1 = float(input("Enter grade for Midterm 1: "))
+mid2 = float(input("Enter grade for Midterm 2: "))
 
-final = float(input("Enter grade for Final Exam:"))
+final = float(input("Enter grade for Final Exam: "))
 
-mid_fin_prep = float(input("Enter grade for Midterms and Final Preparation:"))
+mid_fin_prep = float(input("Enter grade for Midterms and Final Preparation: "))
 
 
 # Here, the weight of each evaluation will be calculated
@@ -29,6 +29,5 @@ w_prep = mid_fin_prep * 0.06
 
 # Here, the overall grade will be calculated
 overall_grade = w_lab + w_quiz + w_assi + w_mid + w_final + w_prep
-int_overall_grade = int(overall_grade) # The final grade has to be an integer, not a float
-print(f"Your grade is: {int_overall_grade}")
-
+rounded_overall_grade = round(overall_grade, 2)
+print(f"Your grade is: {rounded_overall_grade}")
